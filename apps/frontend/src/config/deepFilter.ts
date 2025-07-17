3766e5b72ab0edfd1349095b0f5cb1da11c8d254
pr0d <pavel.erohovets@gmail.com>
2025-07-17 14:20:14 +0300

    DEEPFILTER_ATTEN_LIM = 70

--- apps/frontend/src/config/deepFilter.ts
+++ apps/frontend/src/config/deepFilter.ts
@@ -1,2 +1,2 @@
-export const DEEPFILTER_ATTEN_LIM = 250; // maximum attenuation in dB
+export const DEEPFILTER_ATTEN_LIM = 70; // maximum attenuation in dB
 export const DEEPFILTER_POSTFILTER_BETA = 0.05; // spectral subtraction post-filter 
